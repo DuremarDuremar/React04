fc7e5eb800a8ec32e3d28eb710fbd5082c29af1c
Ivan <duremar98@yandex.ru>
2021-04-30 18:46:36 +0300

    typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
 import React from "react";
-import { useTypedSelector } from "./index";
-import { useDispatch } from "react-redux";
 
 function App() {
-  const dispatch = useDispatch();
-
-  const cash = useTypedSelector((state) => state.cash.cash);
-  console.log(cash);
-
-  const addCash = (value: number) => {
-    dispatch({ type: "ADD_CASH", payload: value });
-  };
-
-  const getCash = (value: number) => {
-    dispatch({ type: "GET_CASH", payload: value });
-  };
-
-  return (
-    <div className="App">
-      <div>{cash}</div>
-      <button onClick={() => addCash(Number(prompt()))}>add</button>
-      <button onClick={() => getCash(Number(prompt()))}>get</button>
-    </div>
-  );
+  return <div className="App">44</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import store, { rootReducers } from "./store/index";
-import { Provider, TypedUseSelectorHook, useSelector } from "react-redux";
 import App from "./App";
-
-type RootState = ReturnType<typeof rootReducers>;
-export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
