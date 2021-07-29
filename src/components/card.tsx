3818fd8623449085573bc23aa330b8a8663576fc
Ivan <duremar98@yandex.ru>
2021-07-29 18:07:24 +0300

    observer

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,29 +1,43 @@
 import React, { FC, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+import { arrowLeft, arrowRight } from "../assets/cards_svg";
 import {
   Content,
   Slider,
   Image,
+  Arrow,
   Title,
   Info,
   Time,
   Cost,
 } from "../style/card_style";
 
-const Card: FC = () => {
-  const [img, setImg] = useState([
-    "https://media.lpgenerator.ru/uploads/2019/07/11/1_thumb600x460.jpg",
-    "https://akket.com/wp-content/uploads/2018/06/Google-Chrome-Android-52.png",
-  ]);
+const images = [
+  "https://media.lpgenerator.ru/uploads/2019/07/11/1_thumb600x460.jpg",
+  "https://akket.com/wp-content/uploads/2018/06/Google-Chrome-Android-52.png",
+  "https://mediasat.info/wp-content/uploads/2015/05/google-logo.jpg",
+];
 
+const Card: FC = () => {
+  const { ref, inView, entry } = useInView({
+    threshold: 0,
+  });
+
+  console.log(inView);
+  console.log(entry?.target.id);
   return (
     <Content>
       <Slider>
-        <Image>
-          <img src={img[0]} alt="0" />
-        </Image>
-        <Image>
-          <img src={img[1]} alt="1" />
-        </Image>
+        {/* <Arrow left>{arrowLeft}</Arrow> */}
+        {images.map((item, index) => {
+          return (
+            <Image key={index} ref={ref} id={String(index)}>
+              <img src={item} alt="30" />
+            </Image>
+          );
+        })}
+        {/* <Arrow>{arrowRight}</Arrow> */}
       </Slider>
       <Title></Title>
       <Info></Info>
--- src/style/card_style.tsx
+++ src/style/card_style.tsx
@@ -1,18 +1,32 @@
 import styled from "styled-components";
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  position: relative;
+`;
 
 export const Slider = styled.div`
   display: flex;
-  overflow-y: hidden;
+  overflow-x: scroll;
 `;
 export const Image = styled.div`
-  /* width: 296px;
-  height: 222px; */
   img {
     width: 296px;
     height: 222px;
   }
+`;
+
+export const Arrow = styled.div<{ left?: boolean }>`
+  background: rgba(0, 0, 0, 0.24);
+  border-radius: 2px;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: ${(props) => (props.left ? "6px" : "250px")};
+  top: 50%;
+  cursor: pointer;
 `;
 
 export const Title = styled.div``;
